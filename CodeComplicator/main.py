63bbdf557b0a2a3ca08188ff09c898a5c5590493
EliasSchramm <privat@eps-dev.de>
2020-08-22 02:29:28 +0200

    Update

---
+++ CodeComplicator/main.py
@@ -0,0 +1,58 @@
+import itertools
+
+global POS, LETTERS, LIST, FUN_DIC, VER_DIC
+
+FUN_DIC = {}
+LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW_"
+POS = 0
+
+
+def getNextName():
+    global POS
+    i = 0
+    for comb in LIST:
+        if (i == POS):
+            name = ""
+            for x in comb:
+                name += x
+            return name
+        i += 1
+    POS += 1
+    return ""
+
+
+def complicatePython(inputfile, outputfile):
+    f = open(inputfile, "r")
+    fileContent = f.read()
+    f.close()
+
+    lines = fileContent.split("\n")
+
+    f = open(outputfile, "a")
+    f.truncate(0)
+
+    for line in lines:
+        if "def " in line and "):" in line:
+            fun_name = line.split("def ")[1].split("(")[0]
+
+            newName = getNextName()
+            FUN_DIC[fun_name] = newName
+
+            line = line.replace(fun_name, newName)
+
+        else:
+            for k, v in FUN_DIC.items():
+                if k in line:
+                    if line.split(k)[1].startswith("("):
+                        line = line.replace(k, v)
+
+        if not line.replace("\n", "").replace(" ", "") == "":
+            f.write(line + "\n")
+
+    f.close()
+
+
+if __name__ == "__main__":
+    LIST = itertools.combinations(LETTERS, 5)
+
+    complicatePython("test/main.py", "test/out.py")
---
+++ CodeComplicator/test/main.py
@@ -0,0 +1,86 @@
+import sys
+from googletrans import Translator, LANGUAGES
+import random
+import time as t
+
+
+def chunks(l, n):
+    n = max(1, n)
+    return (l[i:i + n] for i in range(0, len(l), n))
+
+
+def preprocessString(s):
+    temp = []
+    for x in s:
+        temp.append(' '.join(x))
+    return temp
+
+
+def process(args):
+    input_file = args[0]
+    output_file = args[1]
+    original_language = args[2]
+    iterations = args[3]
+
+    languages = []
+
+    original_text = ''
+    new_text = ''
+
+    save = open(input_file, 'r', encoding="utf-8")
+    original_text = save.read()
+    save.close()
+
+    translator = Translator()
+
+    for language in LANGUAGES:
+        languages.append(language)
+
+    random_choice = random.choice(languages)
+    new_text = translator.translate(original_text, dest=random_choice)
+
+    new_text = new_text.text.split()
+    new_text = list(chunks(new_text, 100))
+
+    print("Translated into " + LANGUAGES[random_choice])
+    for i in range(int(iterations)):
+        random_choice = random.choice(languages)
+
+
+        temp_array = []
+        temp_string = ""
+        for temp_text in new_text:
+            tt = translator.translate(' '.join(temp_text), dest=random_choice).text
+            temp_string += tt + " "
+
+        new_text = ''
+        new_text = temp_string
+        new_text = new_text.split()
+        new_text = list(chunks(new_text, 100))
+
+        print("Translated into " + LANGUAGES[random_choice])
+
+    temp_array = []
+    temp_string = ""
+    j = 0
+    for temp_text in new_text:
+        tt = translator.translate(' '.join(temp_text), dest=original_language).text
+        temp_string += tt + " "
+        print(tt)
+
+    new_text = ''
+    new_text = temp_string
+
+    print(new_text)
+
+    save = open(output_file, 'r+', encoding="utf-8")
+    save.truncate(0)
+    save.write(temp_string)
+    save.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        process(sys.argv[1:])
+    else:
+        print("main.py <input file> <output file> <original language (de, en...) <iterations>")
